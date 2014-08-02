a23c8523457b9b1ffd420d52237cc0e4ccb93047
Zubair AHMED <zubayr.a@gmail.com>
2014-08-02 12:02:09 +0530

    Updated R script.

--- R-Programming/rankhospital.R
+++ R-Programming/rankhospital.R
@@ -1,5 +1,22 @@
 # source : https://github.com/Shekeen/Computing-For-Data-Analysis-Class/tree/master/ProgAssignment2-data
 
+# Ranking hospitals by outcome in a state
+#
+# Write a function called rankhospital that takes three arguments: the 2-character abbreviated name of a
+# state (state), an outcome (outcome), and the ranking of a hospital in that state for that outcome (num).
+# The function reads the outcome-of-care-measures.csv file and returns a character vector with the name
+# of the hospital that has the ranking specified by the num argument. For example, the call
+# rankhospital("MD", "heart failure", 5)
+# would return a character vector containing the name of the hospital with the 5th lowest 30-day death rate
+# for heart failure. The num argument can take values “best”, “worst”, or an integer indicating the ranking
+# (smaller numbers are better). If the number given by num is larger than the number of hospitals in that
+# state, then the function should return NA. Hospitals that do not have data on a particular outcome should
+# be excluded from the set of hospitals when deciding the rankings.
+#
+# Handling ties. It may occur that multiple hospitals have the same 30-day mortality rate for a given cause
+# of death. In those cases ties should be broken by using the hospital name. For example, in Texas (“TX”),
+# the hospitals with lowest 30-day mortality rate for heart failure are shown here.
+
 rankhospital <- function(state, outcome, num = "best") {
     full_data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
     
