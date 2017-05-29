93e1cfc995a0ab1ce6ef0f85f23d12d79b50fb5c
ashtearty <nascapeng@qq.com>
2017-05-29 09:31:03 +0800

    Update complete.R

--- R-Programming/complete.R
+++ R-Programming/complete.R
@@ -40,3 +40,32 @@ complete <- function(directory, id = 1:332) {
 # complete("specdata", c(2, 4, 8, 10, 12))
 # complete("specdata", 30:25)
 # complete("specdata", 3)
+
+
+
+
+complete <- function(directory, id = 1:332){
+    ## 'director' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+    
+    ## Return a data frame of the from:
+    ## id nobs
+    ## 1  117
+    ## 2  1041
+    ## ...
+    ## where 'id' is the monitor ID number and 'nobs' is the
+    ## number of complete cases
+    results <- data.frame(id=numeric(0), nobs=numeric(0))
+    for(monitor in id){
+        path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+        monitor_data <- read.csv(path)
+        interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
+        interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+        nobs <- nrow(interested_data)
+        results <- rbind(results, data.frame(id=monitor, nobs=nobs))
+    }
+    results
+}
